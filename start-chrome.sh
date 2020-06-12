427105d6969d671a6be6587f20786c30909d64de
sych74 <sych74@gmail.com>
2020-06-12 18:14:10 +0300

    replaced DISPLAY by xvfb

--- start-chrome.sh
+++ start-chrome.sh
@@ -5,7 +5,9 @@ server=$1
 video=$2
 [ -z "$video" ] && video="/usr/share/jitsi-meet-torture/resources/FourPeople_1280x720_30.y4m"
 
-DISPLAY=:99 /usr/bin/google-chrome --disable-background-networking --disable-client-side-phishing-detection --disable-default-apps --disable-dev-shm-usage --disable-gpu --disable-hang-monitor --disable-infobars --disable-plugins --disable-popup-blocking \
+#DISPLAY=:99 
+xvfb-run --server-args='-screen 0, 1024x768x24' \
+    /usr/bin/google-chrome --disable-background-networking --disable-client-side-phishing-detection --disable-default-apps --disable-dev-shm-usage --disable-gpu --disable-hang-monitor --disable-infobars --disable-plugins --disable-popup-blocking \
     --disable-prompt-on-repost --disable-sync --disable-web-resources --enable-automation --enable-logging --ignore-certificate-errors --log-level=0 --metrics-recording-only --mute-audio --no-first-run \
     --password-store=basic --use-fake-device-for-media-stream --use-fake-ui-for-media-stream --use-file-for-fake-audio-capture=/usr/share/jitsi-meet-torture/resources/fakeAudioStream.wav --use-file-for-fake-video-capture=$video \
     --use-mock-keychain \
