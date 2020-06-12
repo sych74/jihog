4b65b38851297dace6f95f7c7c5f04d5803f61ec
sych74 <sych74@gmail.com>
2020-06-12 19:29:44 +0300

    add args to start-chrome.sh

--- warmup.sh
+++ warmup.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
+
 set -x 
 
-my_idx=$JIHOG_ME
+my_idx=${1:-JIHOG_ME}
 
 if [ $my_idx -lt 4 ] ;then
-  ./timeout3.sh -t 670 ./start-chrome.sh test_0
+  ./timeout3.sh -t 670 ./start-chrome.sh test_0 $2 $3
   sleep 3
   killall chrome
 elif [ $my_idx -lt 8 ] ;then
-  ./timeout3.sh -t 300 ./start-chrome.sh test_0
+  ./timeout3.sh -t 300 ./start-chrome.sh test_0 $2 $3
   sleep 3
   killall chrome
 else 
-  ./timeout3.sh -t 300 ./start-chrome.sh test_1
+  ./timeout3.sh -t 300 ./start-chrome.sh test_1 $2 $3
   sleep 3
   killall chrome
 fi
 
 if [ $my_idx -lt 8 ] ;then
   if [ $my_idx -gt 3 ] ;then
-    ./timeout3.sh -t 180 ./start-chrome.sh test_2
+    ./timeout3.sh -t 180 ./start-chrome.sh test_2 $2 $3
     sleep 3
     killall chrome
   fi
 elif [ $my_idx -lt 12 ] ;then
-    ./timeout3.sh -t 180 ./start-chrome.sh test_3
+    ./timeout3.sh -t 180 ./start-chrome.sh test_3 $2 $3
     sleep 3
     killall chrome
 else
-    ./timeout3.sh -t 180 ./start-chrome.sh test_4
+    ./timeout3.sh -t 180 ./start-chrome.sh test_4 $2 $3
     sleep 3
     killall chrome
 fi
 
 if [ $my_idx -gt 3 ] ;then
     u=test2_$(($my_idx%6))
-    ./timeout3.sh -t 180 ./start-chrome.sh $u
+    ./timeout3.sh -t 180 ./start-chrome.sh $u $2 $3
     sleep 3
     killall chrome
 fi
 
 u=test3_$(($my_idx%8))
-./timeout3.sh -t 180 ./start-chrome.sh $u
+./timeout3.sh -t 180 ./start-chrome.sh $u $2 $3
 sleep 3
 killall chrome
 
 u=test3_$(($my_idx%2))
-./timeout3.sh -t 180 ./start-chrome.sh $u
+./timeout3.sh -t 180 ./start-chrome.sh $u $2 $3
 sleep 3
 killall chrome
-
