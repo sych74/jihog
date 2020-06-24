96c03fa0dbb9dfe2c30deb8fa1c8a1df1d7ea347
sych74 <sych74@gmail.com>
2020-06-24 08:57:00 +0300

    reduced copy-paste logic

--- warmup.sh
+++ warmup.sh
@@ -4,50 +4,44 @@
 set -x 
 
 my_idx=${1:-JIHOG_ME}
+url=$2
+video=$3
+audio=$4
+
+
+function start(){
+  ./timeout3.sh -t $1 ./start-chrome.sh $2 $url $video $audio
+  sleep 3
+  killall chrome
+}
+
 
 if [ $my_idx -lt 4 ] ;then
-  ./timeout3.sh -t 670 ./start-chrome.sh test_0 $2 $3
-  sleep 3
-  killall chrome
+  start 670 test_0
 elif [ $my_idx -lt 8 ] ;then
-  ./timeout3.sh -t 300 ./start-chrome.sh test_0 $2 $3
-  sleep 3
-  killall chrome
+  start 300 test_0
 else 
-  ./timeout3.sh -t 300 ./start-chrome.sh test_1 $2 $3
-  sleep 3
-  killall chrome
+  start 300 test_1
 fi
 
 if [ $my_idx -lt 8 ] ;then
   if [ $my_idx -gt 3 ] ;then
-    ./timeout3.sh -t 180 ./start-chrome.sh test_2 $2 $3
-    sleep 3
-    killall chrome
+	start 180 test_2
   fi
 elif [ $my_idx -lt 12 ] ;then
-    ./timeout3.sh -t 180 ./start-chrome.sh test_3 $2 $3
-    sleep 3
-    killall chrome
+	start 180 test_3
 else
-    ./timeout3.sh -t 180 ./start-chrome.sh test_4 $2 $3
-    sleep 3
-    killall chrome
+	start 180 test_4
 fi
 
 if [ $my_idx -gt 3 ] ;then
     u=test2_$(($my_idx%6))
-    ./timeout3.sh -t 180 ./start-chrome.sh $u $2 $3
-    sleep 3
-    killall chrome
+	start 180 $u
 fi
 
 u=test3_$(($my_idx%8))
-./timeout3.sh -t 180 ./start-chrome.sh $u $2 $3
-sleep 3
-killall chrome
+start 180 $u
 
 u=test3_$(($my_idx%2))
-./timeout3.sh -t 180 ./start-chrome.sh $u $2 $3
-sleep 3
-killall chrome
+start 180 $u
+
